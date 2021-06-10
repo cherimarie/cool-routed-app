5828db7612fdf4c31f50eed4d8de4dbf9d3e89f4
CheriMarie <cherimarie@gmail.com>
2021-06-10 15:59:19 -0700

    Class activity

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/faq">FAQ</router-link>
   </div>
+
   <router-view/>
+
+
 </template>
 
 <style lang="scss">
---
+++ src/views/Faq.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>FAQ</h1>
+  <p>Lorem ipsum goes here</p>
+</template>
